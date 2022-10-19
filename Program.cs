1970aa4e8c14ad21b9ac220a060abd925eadbdba
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-10-19 23:31:29 +0530

    Added program of logicalprogramming

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Logicalprogramming
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //ReverseNumber.DisplayReverse();
+
+            //ReverseNumber reverseNumber = new ReverseNumber();
+            //reverseNumber.DisplayReverse();
+
+            //Add add = new Add();
+            //add.Sum();
+
+            //Add add1 = new Add(50, 50);
+            //add1.Sum();
+
+            //FibbonacciSeries fibbonacciSeries = new FibbonacciSeries();
+            //fibbonacciSeries.DisplayFibbonacci();
+
+            //PerfectNumber perfect = new PerfectNumber();
+            //PerfectNumber.DisplayPerfectnumber();
+
+            PrimeNumber primeNumber = new PrimeNumber();
+            PrimeNumber.DisplayPrimenumber();
+           
+           
+        }
+    }
+}
