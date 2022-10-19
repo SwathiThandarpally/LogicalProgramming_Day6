48aefc861b37dcf8489ee1c2a386998f8a8e5ca5
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-10-19 23:38:41 +0530

    Added perfect number

---
+++ PerfectNumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logicalprogramming
+{
+    internal class PerfectNumber
+    {
+        public static void DisplayPerfectnumber()
+        {
+            int num, sum = 0, n;
+            Console.WriteLine("Enter a number");
+            num = int.Parse(Console.ReadLine());
+            n = num;
+
+            for (int i = 1; i < num; i++)
+            { 
+                if (num % i == 0)
+                {
+                    sum = sum + i;
+                }
+                if (sum == n)
+                {
+                    Console.WriteLine("\n Enter a number is perfect number");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("\n Enter a number is not  perfect number");
+                    Console.ReadLine();
+                }
+            }
+
+        }
+    }
+}
