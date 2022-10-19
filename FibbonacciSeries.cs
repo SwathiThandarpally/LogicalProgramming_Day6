6d990dca3eed6a44dbee43df40d8ff47367838ca
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-10-19 23:33:56 +0530

    Added fibbonacciseries

---
+++ FibbonacciSeries.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logicalprogramming
+{
+    internal class FibbonacciSeries
+    {
+        public void DisplayFibbonacci()
+        {
+            int firstNumber = 0, secondNumber = 1, thirdNumber = 2, nextNumber, numberOfElements;
+            Console.WriteLine("Enter the number of elements: ");
+            numberOfElements = int.Parse(Console.ReadLine());
+            if(numberOfElements < 0)
+            {
+                Console.WriteLine("Enter a number greater than zero");
+            }
+            else
+            {
+                Console.WriteLine(firstNumber + " " + secondNumber + " " + thirdNumber + " ");
+
+                for (int i =0; i < numberOfElements; i++)
+                {
+                    nextNumber = firstNumber + secondNumber + thirdNumber;
+                    Console.WriteLine(nextNumber + " ");
+                    firstNumber = secondNumber;
+                    secondNumber = thirdNumber;
+                    thirdNumber = nextNumber;
+
+                }
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
