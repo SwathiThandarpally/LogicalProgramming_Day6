c28bf60dd91e5ff4a3cbe6577e752121458af8bf
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-10-19 23:36:32 +0530

    Added reverse series number

---
+++ ReverseNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logicalprogramming
+{
+    public class ReverseNumber
+    {
+        public void DisplayReverse()
+        {
+            int n, reverse = 0, rem;
+            Console.WriteLine("Enter the number");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            while (n != 0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n = n / 10;
+            }
+            Console.WriteLine("Reversed number is " + reverse);
+        }
+
+    }
+}
